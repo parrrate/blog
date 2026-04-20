e177c10b06444ab23653184cb3b71f1037aa8e3a
Alisa Feistel <af@lesb.io>
2026-04-20 20:18:25 +0000

    `VSCODE_PROXY_URI`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { env } from "node:process";
+
+const proxy_url = env.VSCODE_PROXY_URI;
+const allowedHosts =
+	proxy_url ?
+		[
+			proxy_url
+				.replace("{{port}}", "4321")
+				.replace(/^https:\/\//, "")
+				.replace(/\/.*$/, ""),
+		]
+	:	[];
+
+console.log(allowedHosts);
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://blog.parrate.com",
+	server: {
+		allowedHosts,
+	},
 });
